aab2e4ebcf2ee3e08501ad45e4007c6fcf6e5a37
jshiraef <joeshiraef@gmail.com>
2015-01-23 22:36:11 -0500

    Camera movement is now possible, camera now finds player

---
+++ Assets/CameraObject.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraObject : MonoBehaviour {
+
+	private Vector3 movement;
+	private Vector3 position;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		ControlCameraWithInputAxes ();
+
+		//MoveCameraToPlayerLocation ();
+	}
+
+	void ControlCameraWithInputAxes() {
+		float xAxisValue = Input.GetAxis ("Horizontal");
+		float yAxisValue = Input.GetAxis ("Vertical");
+		if (Camera.current != null) {
+				Camera.current.transform.Translate (new Vector3 (xAxisValue, yAxisValue, 0f));
+		}
+	}
+
+	void MoveCameraToPlayerLocation() {
+		
+		// Get the player
+		//Component player = GetComponent("Player") as Component;
+		GameObject player = GameObject.Find("Player");
+		
+		if (Camera.current != null) {
+			Camera.current.transform.position = player.transform.position;
+		}
+	}
+
+}
