dab43767dad40347286ed834ac7b6b005ec465fe
jshiraef <joeshiraef@gmail.com>
2015-01-23 21:07:30 -0500

    began building an isometric setup

---
+++ Assets/Camera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	private Vector3 movement;
+	private Vector3 position;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/StumpProperties.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StumpProperties : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	Renderer.material.color = Color.green
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
