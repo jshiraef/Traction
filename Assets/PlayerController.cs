17e99644d2a51f5be0501c133d967c54070a38d8
Tim <tzorca@users.noreply.github.com>
2015-01-24 11:50:59 -0500

    Got gravity working.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,28 +3,21 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 3f;
+	public float speed = 1000f;
 	public float jumpSpeed;
-	public Vector3 gravity = Vector3.down;
-
-	private bool isJumping;
-	private bool isFalling;
-	private bool isSliding;
-	private bool grounded;
+	public Vector3 gravity = Vector3.down * 0.1f;
+	public Vector3 velocity = Vector3.zero;
 
 	CharacterController controller;
 
-
 	// Use this for initialization
 	void Start () {
-	
-	 controller = GetComponent<CharacterController> ();
-
+		controller = GetComponent<CharacterController> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 		if (Input.GetKey("up"))
 		{
 			transform.Translate (0, 0, (-Time.deltaTime * speed));
@@ -33,37 +26,29 @@ public class PlayerController : MonoBehaviour {
 		{
 			transform.Translate (0, 0, (Time.deltaTime * speed));
 		}
-
+		
 		if (Input.GetKey("left"))
 		{
 			transform.Translate ((Time.deltaTime * speed), 0, 0);
 		}
-
+		
 		if (Input.GetKey("right"))
 		{
 			transform.Translate ((-Time.deltaTime * speed), 0, 0);
 		}
 
 		if (controller.isGrounded) {
-						if (Input.GetKey ("space")) {
-								Debug.Log ("this happened");
-								isJumping = true;
-								controller.Move(Vector3.up * 100f);
-			
-						}
+			if (Input.GetKey ("space")) {
+				controller.Move(Vector3.up * 100f);
+			}
+			velocity.y = 0;
+		} 
+
+		velocity += gravity * Time.deltaTime;
+
+		controller.Move(velocity) ;
 		
-				} else {
-						transform.Translate(gravity * Time.deltaTime) ;
-						}
-
 	}
 
-	void onCollisionEnter3D()
-	{
-		if(collider.gameObject.tag == "Ground floor")
-		{
-			Debug.Log ("this is happening");
-		}
-	}
 
 }
